979aedb06ce339ccf86f0749a1828aa97f2c094a
unknown <vaibhav.kaul2@gnmail.com>
2020-06-02 00:30:22 +0530

    Projects section added

---
+++ js/app.js
@@ -0,0 +1,87 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    document.querySelector('.loader').style.cssText='opacity:0; display:none;';
+    console.log('Body Loaded');
+});
+const navbarOpenBtn=document.querySelector('.navbar_toggler_icon_open');
+const sideNavOpen=document.querySelector('.navbar_top');
+
+document.querySelector('.about').addEventListener('click',()=>{
+
+  document.getElementById('about_section').scrollIntoView();
+});
+document.querySelector('.skills').addEventListener('click',()=>{
+
+  document.getElementById('skills_section').scrollIntoView();
+});
+document.querySelector('.internships').addEventListener('click',()=>{
+
+  document.getElementById('internships_section').scrollIntoView();
+});
+document.querySelector('.projects').addEventListener('click',()=>{
+
+  document.getElementById('projects_section').scrollIntoView();
+});
+document.querySelector('.resume').addEventListener('click',()=>{
+
+  document.getElementById('resume_section').scrollIntoView();
+});
+
+//Navbar open and close function
+navbarOpenBtn.addEventListener('click',()=>{
+  if(navbarOpenBtn.classList.contains('open_side_nav')){
+    sideNavOpen.style.cssText='transform: scale(0);'
+    console.log('Button press');
+    navbarOpenBtn.classList.remove('open_side_nav');
+    navbarOpenBtn.textContent='☰';
+  }
+  else{
+    sideNavOpen.style.cssText='transform: scale(1);'
+    navbarOpenBtn.classList.add('open_side_nav');
+    document.querySelector('.open_side_nav').textContent='X';
+  }
+    
+});
+
+var swiper = new Swiper('.swiper-container', 
+{
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: false,
+  pagination: 
+  {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: 
+  {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+ 
+  breakpoints: 
+  {
+    500:{
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    800: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    }
+  }
+});
+
+
+setInterval(()=>{
+  for(i in document.querySelectorAll(' .swiper-pagination-bullet ')){
+    document.querySelectorAll(' .swiper-pagination-bullet ')[i].innerHTML=i;
+  }
+},1000);
